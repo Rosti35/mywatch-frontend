a6000d7476bd385f1406f165f7724398d8b5cd9e
Ilyas <iokyufokyu@gmail.com>
2024-03-01 23:57:32 +0500

    fix: ios scroll bug

--- src/app/(dashboard)/(main)/layout.tsx
+++ src/app/(dashboard)/(main)/layout.tsx
@@ -2,12 +2,12 @@ import {PropsWithChildren} from 'react';
 
 const LayoutScroll = ({children}: PropsWithChildren) => {
   return (
-    <div className="w-full h-full">
-      <div className="md:pb-[40px] md:pr-[40px] xl:pl-0 md:pt-[40px] p-4">
+    <div className="flex flex-col w-full h-full">
+      <div className="md:pb-[40px] flex flex-col md:pr-[40px] xl:pl-0 md:pt-[40px] p-4">
         {children}
-
-        <div className="h-48" />
       </div>
+
+      <div className="h-48" />
     </div>
   );
 };
